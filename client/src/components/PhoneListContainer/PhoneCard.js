652c3cccd5020434d8dc1cb2f1726d18bfe83cf4
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-23 22:21:52 +0100

    remove propertys from PhoneCard

--- client/src/components/PhoneListContainer/PhoneCard.js
+++ client/src/components/PhoneListContainer/PhoneCard.js
@@ -1,16 +1,14 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export function PhoneCard({ item }) {
   return (
     <div>
-      <Link to={`/phone/${item._id}`}>
+      <Link to={`/phone/${item._id}`} style={{ textDecoration: 'none' }}>
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={item.imageURL} />
           <Card.Body>
             <Card.Title>{item.title}</Card.Title>
-            <Card.Text>{item.price} €</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>
       </Link>
