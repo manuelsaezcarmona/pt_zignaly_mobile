82d6861cbf190d018fb0f4d7aad012b5d87af92e
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-02-24 06:35:58 +0100

    add App.test with test.utils.js Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,9 +10,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={configureStore()}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={configureStore()}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('zignaly')
 );
---
+++ client/src/test.utils.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './redux/store';
+
+function render(component, initialState) {
+  const store = configureStore(initialState);
+
+  const Wrapper = function({ children }) {
+    return (
+      <BrowserRouter>
+        <Provider store={store}>{children}</Provider>
+      </BrowserRouter>
+    );
+  };
+  return rtlRender(component, { wrapper: Wrapper });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render };
